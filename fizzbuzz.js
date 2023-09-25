848c6c51c0da8ce1f6b420571f41e38a70981b15
oliyan <ravisank.p@gmail.com>
2023-09-25 23:25:54 +0530

    added fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+function fizzbuzz(v_num) {
+  if (v_num % 3 == 0 && v_num % 5==0) // divisible by 3 & 5
+  return 'fizzbuzz';
+  
+  if (v_num % 3 == 0) // divisible by 3
+  return 'fizz';
+  
+  if (v_num % 5 == 0) // divisible by 5
+  return 'buzz';
+  
+  if (typeof(v_num) !== 'number' ) // not a number
+  return 'Not a Number'
+  
+  return v_num; // return the number if all else
+
+}
+
+let num = 30;
+console.log(fizzbuzz(num));
